5c26a39f441ddb52b2afe9f8d7c07e097e823f88
wagwbot <76049451+wagwbot@users.noreply.github.com>
2020-12-16 13:40:29 +0700

    Add files via upload

---
+++ donate.js
@@ -0,0 +1,23 @@
+exports.donate = (id, BotName, tampilTanggal, tampilWaktu, instagramlu, whatsapplu, kapanbotaktif, grupch1, grupch2) => {
+	return `✨ -----[ *DONASI ${BotName}* ]----- ✨
+  
+Hi, ${id.split("@s.whatsapp.net")[0]} 👋️
+
+Mau donasi? ✨
+
+🗓️️ *${tampilTanggal}*
+🕘 *${tampilWaktu}*
+
+♻ Silahkan donasi dibawah ini :
+   
+
+🖼️ *FOLLOW*: riska_gaming._
+
+🖼️ *PULSA*: 0895355566000
+
+🖼️ *SUBSCRIBE*:RISKA_GAMING
+
+
+  
+✨ -----[ *POWERED BY ${RAMA}* ]----- ✨`
+}
